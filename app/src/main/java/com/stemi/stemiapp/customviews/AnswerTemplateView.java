9e8e5ad5a286d4ba36c7c358e6ddb407ebf4acb8
praburaam <prabu.raam@agiliztech.com>
2017-07-17 18:12:17 +0530

    Custom view for getting answer response

---
+++ app/src/main/java/com/stemi/stemiapp/customviews/AnswerTemplateView.java
@@ -0,0 +1,82 @@
+package com.stemi.stemiapp.customviews;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.stemi.stemiapp.R;
+
+/**
+ * Created by praburaam on 17/07/17.
+ */
+
+public class AnswerTemplateView extends LinearLayout implements View.OnClickListener {
+    private TextView tv_yes, tv_no, tv_dont_know;
+    private String response;
+
+    public AnswerTemplateView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        View v = LayoutInflater.from(context).inflate(R.layout.answer_layout, null);
+        tv_yes = (TextView) v.findViewById(R.id.tv_yes);
+        tv_no = (TextView) v.findViewById(R.id.tv_no);
+        tv_dont_know = (TextView) v.findViewById(R.id.tv_dont_know);
+
+        tv_yes.setOnClickListener(this);
+        tv_no.setOnClickListener(this);
+        tv_dont_know.setOnClickListener(this);
+
+        addView(v, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT));
+
+    }
+
+
+    public String getResponse() {
+        return response;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.tv_yes:
+                response = "YES";
+                tv_no.setBackground(getResources().getDrawable(R.drawable.text_border_with_color));
+                //tv_no.setBackgroundColor(getResources().getColor(R.color.appBackground));
+                tv_no.setTextColor(getResources().getColor(R.color.white));
+
+                tv_dont_know.setBackground(getResources().getDrawable(R.drawable.text_border_with_color));
+                tv_dont_know.setTextColor(getResources().getColor(R.color.white));
+
+                tv_yes.setBackgroundColor(getResources().getColor(R.color.white));
+                tv_yes.setTextColor(getResources().getColor(R.color.appBackground));
+                break;
+            case R.id.tv_no:
+                response = "NO";
+                tv_yes.setBackground(getResources().getDrawable(R.drawable.text_border_with_color));
+                tv_yes.setTextColor(getResources().getColor(R.color.white));
+
+                tv_dont_know.setBackground(getResources().getDrawable(R.drawable.text_border_with_color));
+                tv_dont_know.setTextColor(getResources().getColor(R.color.white));
+
+                tv_no.setBackgroundColor(getResources().getColor(R.color.white));
+                tv_no.setTextColor(getResources().getColor(R.color.appBackground));
+                break;
+            case R.id.tv_dont_know:
+                response = "DON'T KNOW";
+                tv_no.setBackground(getResources().getDrawable(R.drawable.text_border_with_color));
+                tv_no.setTextColor(getResources().getColor(R.color.white));
+
+                tv_yes.setBackground(getResources().getDrawable(R.drawable.text_border_with_color));
+                tv_yes.setTextColor(getResources().getColor(R.color.white));
+
+                tv_dont_know.setBackgroundColor(getResources().getColor(R.color.white));
+                tv_dont_know.setTextColor(getResources().getColor(R.color.appBackground));
+                break;
+        }
+
+    }
+}
