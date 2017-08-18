9b3de2cb8a7972ab7ac7ea3d803c9d90d3adca53
praburaam <prabu.raam@agiliztech.com>
2017-08-18 14:30:17 +0530

    Adding location picking feature to SOS screen

--- app/src/main/java/com/stemi/stemiapp/fragments/SOSFragment.java
+++ app/src/main/java/com/stemi/stemiapp/fragments/SOSFragment.java
@@ -1,22 +1,36 @@
 package com.stemi.stemiapp.fragments;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 import com.stemi.stemiapp.R;
 import com.stemi.stemiapp.activity.TrackActivity;
 import com.stemi.stemiapp.customviews.BetterSpinner;
-import com.stemi.stemiapp.customviews.SpinnerWithHint;
 import com.stemi.stemiapp.databases.DBforUserDetails;
 
 import java.util.ArrayList;
 
-import static android.content.ContentValues.TAG;
+import static android.app.Activity.RESULT_OK;
 
 
 /**
@@ -27,6 +41,12 @@ public class SOSFragment extends Fragment {
     DBforUserDetails dBforUserDetails;
     BetterSpinner personSpinner;
     ArrayList<String> personName;
+    private ImageView pick_location;
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private static final String TAG = "SOSFragment";
+    int PLACE_PICKER_REQUEST = 1;
+    private EditText et_location;
+
     public SOSFragment() {
         // Required empty public constructor
     }
@@ -42,9 +62,18 @@ public class SOSFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_sos, container, false);
         personSpinner = (BetterSpinner) view.findViewById(R.id.person_Spinner);
+        pick_location = (ImageView) view.findViewById(R.id.pick_location);
+        et_location = (EditText)view.findViewById(R.id.et_location);
         dBforUserDetails = new DBforUserDetails(getActivity());
         personName = new ArrayList<>();
         personName = dBforUserDetails.getRecords();
+
+        pick_location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkLocationServices();
+            }
+        });
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getActivity(), R.layout.spinner_layout, personName );
         personSpinner.setAdapter(arrayAdapter);
@@ -62,5 +91,94 @@ public class SOSFragment extends Fragment {
             }
         }
         super.setMenuVisibility(menuVisible);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, getActivity());
+                String toastMsg = String.format("Place: %s", place.getAddress());
+                et_location.setText(place.getAddress());
+            }
+        }
+    }
+
+    private void openPlacePicker(){
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+
+        try {
+            if(checkPlayServices()){
+                startActivityForResult(builder.build(getActivity())
+
+                        , PLACE_PICKER_REQUEST);
+            }
+
+        } catch (GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        }
+    }
+    private boolean checkPlayServices() {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(getActivity());
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (apiAvailability.isUserResolvableError(resultCode)) {
+                apiAvailability.getErrorDialog(getActivity(), resultCode, PLAY_SERVICES_RESOLUTION_REQUEST)
+                        .show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                //finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    private void checkLocationServices(){
+        LocationManager lm = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
+        boolean gps_enabled = false;
+        boolean network_enabled = false;
+
+        try {
+            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        } catch(Exception ex) {}
+
+        try {
+            network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        } catch(Exception ex) {}
+
+        if(!gps_enabled && !network_enabled) {
+            // notify user
+            AlertDialog.Builder dialog;
+            // dialog = new AlertDialog.Builder(this);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                dialog = new AlertDialog.Builder(getActivity(), android.R.style.Theme_Material_Light_Dialog_Alert);
+            } else {
+                dialog = new AlertDialog.Builder(getActivity());
+            }
+            dialog.setMessage(getResources().getString(R.string.gps_network_not_enabled));
+            dialog.setPositiveButton(getResources().getString(R.string.open_location_settings), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    // TODO Auto-generated method stub
+                    Intent myIntent = new Intent( Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(myIntent);
+                    //get gps
+                }
+            });
+            dialog.setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    // TODO Auto-generated method stub
+
+                }
+            });
+            dialog.show();
+        }
+        else{
+            openPlacePicker();
+        }
     }
 }
