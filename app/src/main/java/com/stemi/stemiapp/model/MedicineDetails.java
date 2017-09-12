ddf127debcd3dfba46382bd6e8fc6299924c8e0e
poojashree <pooja@agiliztech.com>
2017-09-12 14:43:06 +0530

    Splash Screen, Onboarding screen, Medicine Details screen is added

--- app/src/main/java/com/stemi/stemiapp/model/MedicineDetails.java
+++ app/src/main/java/com/stemi/stemiapp/model/MedicineDetails.java
@@ -4,5 +4,114 @@ package com.stemi.stemiapp.model;
  * Created by Pooja on 06-09-2017.
  */
 
-public class MedicineDetails {
+public class MedicineDetails  {
+
+    private String personName;
+    private   String medicineName;
+    private  String medicineType;
+    private int medicineColor;
+    private  String medicineMorning;
+    private String medicineMorningTime;
+    private  String medicineAfternoon;
+    private   String medicineNoonTime;
+    private  String medicineNight;
+    private  String medicineNightTime;
+    private  String medicineDays;
+    private  Boolean medicineRemainder;
+
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    public String getMedicineName() {
+        return medicineName;
+    }
+
+    public void setMedicineName(String medicineName) {
+        this.medicineName = medicineName;
+    }
+
+    public String getMedicineType() {
+        return medicineType;
+    }
+
+    public void setMedicineType(String medicineType) {
+        this.medicineType = medicineType;
+    }
+
+    public int getMedicineColor() {
+        return medicineColor;
+    }
+
+    public void setMedicineColor(int medicineColor) {
+        this.medicineColor = medicineColor;
+    }
+
+    public String getMedicineMorning() {
+        return medicineMorning;
+    }
+
+    public void setMedicineMorning(String medicineMorning) {
+        this.medicineMorning = medicineMorning;
+    }
+
+    public String getMedicineMorningTime() {
+        return medicineMorningTime;
+    }
+
+    public void setMedicineMorningTime(String medicineMorningTime) {
+        this.medicineMorningTime = medicineMorningTime;
+    }
+
+    public String getMedicineAfternoon() {
+        return medicineAfternoon;
+    }
+
+    public void setMedicineAfternoon(String medicineAfternoon) {
+        this.medicineAfternoon = medicineAfternoon;
+    }
+
+    public String getMedicineNoonTime() {
+        return medicineNoonTime;
+    }
+
+    public void setMedicineNoonTime(String medicineNoonTime) {
+        this.medicineNoonTime = medicineNoonTime;
+    }
+
+    public String getMedicineNight() {
+        return medicineNight;
+    }
+
+    public void setMedicineNight(String medicineNight) {
+        this.medicineNight = medicineNight;
+    }
+
+    public String getMedicineNightTime() {
+        return medicineNightTime;
+    }
+
+    public void setMedicineNightTime(String medicineNightTime) {
+        this.medicineNightTime = medicineNightTime;
+    }
+
+    public String getMedicineDays() {
+        return medicineDays;
+    }
+
+    public void setMedicineDays(String medicineDays) {
+        this.medicineDays = medicineDays;
+    }
+    public Boolean getMedicineRemainder() {
+        return medicineRemainder;
+    }
+
+    public void setMedicineRemainder(Boolean medicineRemainder) {
+        this.medicineRemainder = medicineRemainder;
+    }
 }
