c454a384437a3b1c488a97ffbba8decf952ac825
poojashree <pooja@agiliztech.com>
2017-10-24 12:19:09 +0530

    Edit medicine issue is fixed

---
+++ app/src/main/java/com/stemi/stemiapp/activity/PrivacyActivity.java
@@ -0,0 +1,15 @@
+package com.stemi.stemiapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.stemi.stemiapp.R;
+
+public class PrivacyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacy);
+    }
+}
