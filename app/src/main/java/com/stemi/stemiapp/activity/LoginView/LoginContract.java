aecdd9613f7c855a85d11810674e7c20d0ce11d3
poojashree <pooja@agiliztech.com>
2018-01-31 11:42:03 +0530

    LoginMVP

---
+++ app/src/main/java/com/stemi/stemiapp/activity/LoginView/LoginContract.java
@@ -0,0 +1,14 @@
+package com.stemi.stemiapp.activity.LoginView;
+
+/**
+ * Created by Pooja on 09-01-2018.
+ */
+
+public interface LoginContract  {
+    interface LoginView {
+
+    }
+    interface  LoginPresenter{
+
+    }
+}
---
+++ app/src/main/java/com/stemi/stemiapp/activity/LoginView/LoginPresenter.java
@@ -0,0 +1,12 @@
+package com.stemi.stemiapp.activity.LoginView;
+
+import android.content.Context;
+import android.text.TextUtils;
+
+/**
+ * Created by Pooja on 09-01-2018.
+ */
+
+public class LoginPresenter implements LoginContract.LoginPresenter  {
+    LoginContract.LoginView registerView;
+}
